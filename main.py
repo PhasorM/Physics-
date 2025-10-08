91aa4c663bf1b1a740f5743951f88b2b6d4af77a
PhasorM <74255289+PhasorM@users.noreply.github.com>
2025-10-09 03:35:25 +0530

    Update main.py
    Displays a 2D "Trampoline" made with the updated Link function

--- main.py
+++ main.py
@@ -13,60 +13,74 @@ running = True
 l = []
 dt=0.1
 ac=Vector2D(0,0.98)
-for i in range(0):
-    #(self, radius: float , position:Vector2D,  position_old: Vector2D,  acceleration: Vector2D = (0,0), mass: float =1.0 )
-    r=20
-    v_in = Vector2D(random.randint(-5,5),random.randint(-5,5))
-    pos=Vector2D(random.randint(r,a-r),random.randint(r,b-r))
+for i in range(30):
+    r=10
+    v_in = Vector2D(5,random.randint(-5,5))
+    pos=Vector2D(random.randint(r,a-r),random.randint(r,int(b/4)))
     old_pos= pos - v_in * dt
-    #ac = Vector2D(0,0)
     l.append(entity(r,
     pos, 
-    old_pos,
-    ac))
+    old_pos))
 l1=[]
-h=200
-for i in range(0,2):
-    #(self, radius: float , position:Vector2D,  position_old: Vector2D,  acceleration: Vector2D = (0,0), mass: float =1.0 )
-    r=10
+h=-100
+d=10
+number= 30
+for i in range(number):
+    r=5
     v_in = Vector2D(0,0)
     pos=Vector2D(h,300)
-    old_pos= pos - v_in * dt
-    if (i==0):
-        ac = Vector2D(0,0)
-    else : 
-        ac = Vector2D(0,0.98)
+    old_pos= pos
+
     l1.append(entity(r,
     pos,
-    old_pos,
-    ac))
-    h=h+25
-
+    old_pos))
+    h=h+d
+    print(h)
+end_pos = h + (number - 1)*d
 l=l1+l
 
-
-substeps = 8
+substeps =2
+trajectory = []
 while running:
     for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
-                running = False
+        if (event.type == pygame.QUIT):
+            running = False
     screen.fill((0,0,0))
-    pygame.draw.line(screen, (255, 255, 255), (0, 300-r), (800, 300-r), 1)
     
-    
-    
-    for i in range(len(l)):
-        for s in range(substeps):
-            l[i].WallCollision(a-r,b-r,r,r,dt/substeps)
-            l[i].accelerate(ac)        
-            for j in range(i+1,len(l)):
-                l[i].Collision(l[j],1)
-            
+    for j in range(substeps):
+        for i in range(len(l)):
+            l[i].accelerate(ac)
+            l[i].WallCollision(a-r,b-r,r,r,dt/substeps,0.9)
+            for k in range(i+1,len(l)):
+                if (not( i <= len(l1)-1 and k <= len(l1)-1)):
+                    l[i].Collision(l[k],0.9)
             l[i].update(dt/substeps)
-            
-            pygame.draw.circle(screen, (255, 0, 0), (int(l[i].position.x), int(l[i].position.y)), r)
-    
-
-    screen.blit
+            if (i<=len(l1)-1):        
+                pygame.draw.circle(screen, (255, 0, 0), (int(l[i].position.x), int(l[i].position.y)), l[i].radius)
+            else:
+                pygame.draw.circle(screen, (0, 255, 0), (int(l[i].position.x), int(l[i].position.y)), l[i].radius)
+        l1[0].position=Vector2D(h,300)
+        l1[len(l1)-1].position=Vector2D(end_pos,300)
+        for iterations in range(5):
+            for i in range(len(l1)-1):
+                # dist = (l1[i].position - l1[i+1].position).ab()
+                # difference = dist - d
+                # normal = (l1[i].position - l1[i+1].position).norm()
+                # delta = normal * (difference)
+                if (i != 0 or i!= len(l1)-2):
+                    # l1[i].position = l1[i].position - delta * 0.5
+                    # l1[i+1].position = l1[i+1].position + delta * 0.5
+                    l[i].Link(l[i+1],d,"free")
+                elif (i == 0):
+                    # l1[i+1].position = l1[i+1].position + delta
+                    l[i].Link(l[i+1],d,"pin")
+                elif (i == len(l1)-1):
+                    # l1[i-1].position = l1[i-1].position + delta
+                    l[i].Link(l[i-1],d,"pin")
+        
     pygame.display.update()
+        
+
+
+
 
